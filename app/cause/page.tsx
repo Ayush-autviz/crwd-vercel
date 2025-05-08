42b9813a886bf1f8a6cc7fc84123206b5438f89d
Shubham-autviz3927 <shubham@autviz.in>
2025-05-08 18:08:42 +0530

    updated

--- app/cause/page.tsx
+++ app/cause/page.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import CauseHeader from '@/components/cause/CauseHeader'; 
 import CauseProfileCard from '@/components/cause/CauseProfileCard';
+import CauseCommunityHighlights from '@/components/cause/CauseCommunityHighlights';
 import CauseRecentDonations from '@/components/cause/CauseRecentDonations';
 import CauseAboutCard from '@/components/cause/CauseAboutCard';
 import CauseDonateBar from '@/components/cause/CauseDonateBar';
+import HighLights from '@/components/cause/highlight/HighLights';
 
 const CausePage: React.FC = () => (
   <div className=" min-h-screen md:h-full bg-white flex flex-col pb-28">
     <CauseHeader />
-    <div className="flex flex-col space-y-6 py-4s">
-      <CauseProfileCard />
+    <div className="flex flex-col space-y-6 py-4">
+      <CauseProfileCard />  
+      <HighLights/>
       <CauseRecentDonations />
       <CauseAboutCard />
     </div>
